33f303b435083e72184c4b60315b63b00d9f66c0
Wojciech <hujciwdupe@wp.pl>
2013-08-10 10:40:05 -0600

    Databasebroker

--- src/Broker/DatabaseBroker.java
+++ src/Broker/DatabaseBroker.java
@@ -4,13 +4,58 @@
  */
 package Broker;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author WojG
  */
 public class DatabaseBroker
 {
+
     private String host = "jdbc:mysql://localhost:3306/asset management";
     private String username = "root";
     private String password = "P@$$w0rd";
+    private final int MAX_POOL = 5;
+    private boolean connected;
+    private Queue<Connection> connectionPool = new LinkedList();
+
+    public DatabaseBroker()
+    {
+        createConnectionPool();
+    }
+
+    // establish connection with database
+    private Connection createNewConnection()
+    {
+        Connection connect = null;
+        try
+        {
+            connect = DriverManager.getConnection(host, username, username);
+            connected = true;
+        }
+        catch (SQLException err)
+        {
+
+            System.out.println("Error trying to establish connection with db: " + err.getMessage());
+            connected = false;
+        }
+        return connect;
+    }
+    // create specified number of connections
+
+    private void createConnectionPool()
+    {
+        while (connectionPool.size() < MAX_POOL)
+        {
+            connectionPool.add(createNewConnection());
+        }
+        System.out.println("Pool allocation: " + connectionPool.size());
+    }
+    // use connection
+    // put back connection
 }
--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -4,13 +4,7 @@
  */
 package inventory;
 
-import java.io.*;
-import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.ss.usermodel.*;
+import Broker.DatabaseBroker;
 
 /**
  *
@@ -24,58 +18,6 @@ public class Inventory
      */
     public static void main(String[] args)
     {
-        try
-        {
-            FileInputStream input = new FileInputStream(new File("2010 System.xls"));
-
-            //Get the workbook instance for XLS file 
-            HSSFWorkbook my_xls_workbook = new HSSFWorkbook(input);
-
-            //Get first sheet from the workbook
-            HSSFSheet my_worksheet = my_xls_workbook.getSheetAt(2);
-
-            //Iterate through each rows from first sheet
-            Iterator<Row> rowIterator = my_worksheet.iterator();
-            /*  FileWriter my_csv = new FileWriter("convertedCSVFile.csv");
-             CSVWriter my_csv_output = new CSVWriter(my_csv);*/
-
-            while (rowIterator.hasNext())
-            {
-                Row row = rowIterator.next();
-
-                //For each row, iterate through each columns
-                Iterator<Cell> cellIterator = row.cellIterator();
-                System.out.println();
-                while (cellIterator.hasNext())
-                {
-                    //Fetch CELL
-                    Cell cell = cellIterator.next();
-
-                    switch (cell.getCellType())
-                    {
-                        case Cell.CELL_TYPE_STRING:
-                            System.out.print(cell.getStringCellValue() + " | ");
-                            break;
-                        case Cell.CELL_TYPE_NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + " | ");
-                            break;
-                    }
-                }
-            }
-            //      my_csv_output.close(); //close the CSV file
-            //we created our file..!!
-            //   FileOutputStream out = new FileOutputStream(new File("test.xls"));
-            //   my_xls_workbook.write(out);
-            input.close(); //close xls 
-
-        }
-        catch (FileNotFoundException ex)
-        {
-            Logger.getLogger(Inventory.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        catch (IOException ex)
-        {
-            Logger.getLogger(Inventory.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        DatabaseBroker dbB = new DatabaseBroker();
     }
 }
